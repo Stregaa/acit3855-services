9a33615691c281275c3ce0e7986133c71e33c298
stregaa <markus_chung@hotmail.com>
2022-11-21 20:06:51 -0800

    ✨ lab 10 part 1

--- receiver/app.py
+++ receiver/app.py
@@ -10,17 +10,39 @@ import json
 import time
 from pykafka import KafkaClient
 
-current_datetime = datetime.datetime.now()
-current_datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+import os
 
-with open("app_conf.yml", "r") as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open("log_conf.yml", "r") as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
-logger = logging.getLogger("basicLogger")
+logger = logging.getLogger('basicLogger')
+
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
+# with open("app_conf.yml", "r") as f:
+#     app_config = yaml.safe_load(f.read())
+
+# with open("log_conf.yml", "r") as f:
+#     log_config = yaml.safe_load(f.read())
+#     logging.config.dictConfig(log_config)
+
+current_datetime = datetime.datetime.now()
+current_datetime_str = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
 headers = {"Content-Type": "application/json"}
 # MAX_EVENTS = 10
